structured-world/strongswan

commit 21b33aa04b6ce476f3548a13ec8ab3054ce7e9bc

libtls: call create_public_enumerator() with key_type

src/libtls/tls_server.c
@@ -176,14 +176,21 @@ public_key_t *tls_find_public_key(auth_cfg_t *peer_auth, identification_t *id)
 {
 	public_key_t *public = NULL, *current;
 	certificate_t *cert, *found;
+	key_type_t key_type = KEY_ANY;
 	enumerator_t *enumerator;
 	auth_cfg_t *auth;
 
 	cert = peer_auth->get(peer_auth, AUTH_HELPER_SUBJECT_CERT);
 	if (cert)
 	{
+		public = cert->get_public_key(cert);
+		if (public)
+		{
+			key_type = public->get_type(public);
+			public->destroy(public);
+		}
 		enumerator = lib->credmgr->create_public_enumerator(lib->credmgr,
-												KEY_ANY, id, peer_auth, TRUE);
+											key_type, id, peer_auth, TRUE);
 		while (enumerator->enumerate(enumerator, &current, &auth))
 		{
 			found = auth->get(auth, AUTH_RULE_SUBJECT_CERT);

commit d8c0d966d8ffdb4b9e41cd70b2fece486464b321

libtls: Fix double-free for untrusted peer certificates
`public` is returned, but previously only if a trusted key was found.
We obviously don't want to return untrusted keys and since the reference
was correctly destroyed after determining the key type, this later caused
a double-free.

Fixes: 21b33aa04b6c ("libtls: call create_public_enumerator() with key_type")

src/libtls/tls_server.c
@@ -183,11 +183,11 @@ public_key_t *tls_find_public_key(auth_cfg_t *peer_auth, identification_t *id)
 	cert = peer_auth->get(peer_auth, AUTH_HELPER_SUBJECT_CERT);
 	if (cert)
 	{
-		public = cert->get_public_key(cert);
-		if (public)
+		current = cert->get_public_key(cert);
+		if (current)
 		{
-			key_type = public->get_type(public);
-			public->destroy(public);
+			key_type = current->get_type(current);
+			current->destroy(current);
 		}
 		enumerator = lib->credmgr->create_public_enumerator(lib->credmgr,
 											key_type, id, peer_auth, TRUE);