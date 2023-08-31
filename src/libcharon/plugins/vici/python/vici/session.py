structured-world/strongswan

commit f6a5bf531f743386c06508b40bac2f8830f421a6

vici: Create default TCP socket on Windows in Python bindings
This uses the same value as VICI_DEFAULT_URI.

References strongswan/strongswan#1655

src/libcharon/plugins/vici/python/vici/session.py
@@ -1,4 +1,5 @@
 import socket
+import platform
 
 from .exception import SessionException, CommandException, EventUnknownException
 from .protocol import Transport, Packet, Message, RECV_TIMEOUT_DEFAULT
@@ -26,8 +27,12 @@ class Session(CommandWrappers, object):
         :type sock: socket.socket
         """
         if sock is None:
-            sock = socket.socket(socket.AF_UNIX)
-            sock.connect("/var/run/charon.vici")
+            if platform.system() == "Windows":
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect('127.0.0.1', 4502)
+            else:
+                sock = socket.socket(socket.AF_UNIX)
+                sock.connect("/var/run/charon.vici")
         self.transport = Transport(sock)
 
     def _communicate(self, packet):

commit f652d9b9f6351a8d33c4cd9366f480fa972d34f5

vici: Fix connecting to socket in Python bindings on Windows
socket.connect() takes a single argument with the address that depends
on the address family, for TCP it's a tuple with IP and port.

Fixes: f6a5bf531f74 ("vici: Create default TCP socket on Windows in Python bindings")
Closes strongswan/strongswan#1874

src/libcharon/plugins/vici/python/vici/session.py
@@ -29,7 +29,7 @@ class Session(CommandWrappers, object):
         if sock is None:
             if platform.system() == "Windows":
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect('127.0.0.1', 4502)
+                sock.connect(('127.0.0.1', 4502))
             else:
                 sock = socket.socket(socket.AF_UNIX)
                 sock.connect("/var/run/charon.vici")