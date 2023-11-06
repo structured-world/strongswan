structured-world/strongswan

commit 49a882644da1d644e1e837edb541fcdef4b6fe63

ikev2: Trigger ike_updown() event after all IKE-specific tasks ran
This makes sure the event is only triggered after the IKE_SA is fully
established and e.g. virtual IPs, additional peer addresses or
a modified reauth time (on the initiator) are assigned to it.  This was
e.g. a problem for the selinux plugin if virtual IPs are used.

We use a separate task to trigger the event that's queued before the
child-create task so the event is triggered before the child_updown()
event.  Same goes for the state change to IKE_ESTABLISHED.

A new condition is used to indicate the successful completion of all
authentication rounds, so we don't have to set the IKE_ESTABLISHED state
in the ike-auth task (it was used as condition in other tasks).

Since set_state() also sets the rekey and reauth times, this required
some minor changes in regards to how AUTH_LIFETIME notifies are handled.

src/libcharon/sa/ike_sa.c
@@ -893,6 +893,11 @@ METHOD(ike_sa_t, set_state, void,
 					lib->scheduler->schedule_job(lib->scheduler, job, t);
 					DBG1(DBG_IKE, "scheduling reauthentication in %ds", t);
 				}
+				else if (this->stats[STAT_REAUTH])
+				{
+					t = this->stats[STAT_REAUTH] - this->stats[STAT_ESTABLISHED];
+					DBG1(DBG_IKE, "reauthentication already scheduled in %ds", t);
+				}
 				t = this->peer_cfg->get_over_time(this->peer_cfg);
 				if (this->stats[STAT_REKEY] || this->stats[STAT_REAUTH])
 				{

src/libcharon/Android.mk
@@ -103,6 +103,7 @@ sa/ikev2/tasks/ike_cert_post.c sa/ikev2/tasks/ike_cert_post.h \
 sa/ikev2/tasks/ike_config.c sa/ikev2/tasks/ike_config.h \
 sa/ikev2/tasks/ike_delete.c sa/ikev2/tasks/ike_delete.h \
 sa/ikev2/tasks/ike_dpd.c sa/ikev2/tasks/ike_dpd.h \
+sa/ikev2/tasks/ike_establish.c sa/ikev2/tasks/ike_establish.h \
 sa/ikev2/tasks/ike_init.c sa/ikev2/tasks/ike_init.h \
 sa/ikev2/tasks/ike_natd.c sa/ikev2/tasks/ike_natd.h \
 sa/ikev2/tasks/ike_mid_sync.c sa/ikev2/tasks/ike_mid_sync.h \

src/libcharon/Makefile.am
@@ -102,6 +102,7 @@ sa/ikev2/tasks/ike_cert_post.c sa/ikev2/tasks/ike_cert_post.h \
 sa/ikev2/tasks/ike_config.c sa/ikev2/tasks/ike_config.h \
 sa/ikev2/tasks/ike_delete.c sa/ikev2/tasks/ike_delete.h \
 sa/ikev2/tasks/ike_dpd.c sa/ikev2/tasks/ike_dpd.h \
+sa/ikev2/tasks/ike_establish.c sa/ikev2/tasks/ike_establish.h \
 sa/ikev2/tasks/ike_init.c sa/ikev2/tasks/ike_init.h \
 sa/ikev2/tasks/ike_natd.c sa/ikev2/tasks/ike_natd.h \
 sa/ikev2/tasks/ike_mid_sync.c sa/ikev2/tasks/ike_mid_sync.h \

src/libcharon/sa/ike_sa.h
@@ -245,6 +245,11 @@ enum ike_condition_t {
 	 * A Postquantum Preshared Key was used when this IKE_SA was created
 	 */
 	COND_PPK = (1<<13),
+
+	/**
+	 * All authentication rounds have been completed successfully
+	 */
+	COND_AUTHENTICATED = (1<<14),
 };
 
 /**

src/libcharon/sa/ikev2/task_manager_v2.c
@@ -38,6 +38,7 @@
 #include <sa/ikev2/tasks/ike_mid_sync.h>
 #include <sa/ikev2/tasks/ike_vendor.h>
 #include <sa/ikev2/tasks/ike_verify_peer_cert.h>
+#include <sa/ikev2/tasks/ike_establish.h>
 #include <sa/ikev2/tasks/child_create.h>
 #include <sa/ikev2/tasks/child_rekey.h>
 #include <sa/ikev2/tasks/child_delete.h>
@@ -554,6 +555,8 @@ METHOD(task_manager_t, initiate, status_t,
 					activate_task(this, TASK_IKE_CONFIG);
 					activate_task(this, TASK_IKE_AUTH_LIFETIME);
 					activate_task(this, TASK_IKE_MOBIKE);
+					/* make sure this is the last IKE-related task */
+					activate_task(this, TASK_IKE_ESTABLISH);
 					activate_task(this, TASK_CHILD_CREATE);
 				}
 				break;
@@ -1151,9 +1154,14 @@ static status_t process_request(private_task_manager_t *this,
 #endif /* ME */
 				task = (task_t*)ike_config_create(this->ike_sa, FALSE);
 				array_insert(this->passive_tasks, ARRAY_TAIL, task);
-				task = (task_t*)ike_auth_lifetime_create(this->ike_sa, FALSE);
-				array_insert(this->passive_tasks, ARRAY_TAIL, task);
 				task = (task_t*)ike_mobike_create(this->ike_sa, FALSE);
+				array_insert(this->passive_tasks, ARRAY_TAIL, task);
+				/* this should generally be the last IKE-related task */
+				task = (task_t*)ike_establish_create(this->ike_sa, FALSE);
+				array_insert(this->passive_tasks, ARRAY_TAIL, task);
+				/* make sure this comes after the above task to send the correct
+				 * reauth time, as responder the task doesn't modify it anymore */
+				task = (task_t*)ike_auth_lifetime_create(this->ike_sa, FALSE);
 				array_insert(this->passive_tasks, ARRAY_TAIL, task);
 				task = (task_t*)child_create_create(this->ike_sa, NULL, FALSE,
 													NULL, NULL);
@@ -2128,6 +2136,10 @@ METHOD(task_manager_t, queue_ike, void,
 		{
 			queue_task(this, (task_t*)ike_mobike_create(this->ike_sa, TRUE));
 		}
+	}
+	if (!has_queued(this, TASK_IKE_ESTABLISH))
+	{
+		queue_task(this, (task_t*)ike_establish_create(this->ike_sa, TRUE));
 	}
 #ifdef ME
 	if (!has_queued(this, TASK_IKE_ME))

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -1552,7 +1552,7 @@ METHOD(task_t, build_r, status_t,
 			no_dh = FALSE;
 			break;
 		case IKE_AUTH:
-			if (this->ike_sa->get_state(this->ike_sa) != IKE_ESTABLISHED)
+			if (!this->ike_sa->has_condition(this->ike_sa, COND_AUTHENTICATED))
 			{	/* wait until all authentication round completed */
 				return NEED_MORE;
 			}
@@ -1762,7 +1762,7 @@ METHOD(task_t, process_i, status_t,
 			no_dh = FALSE;
 			break;
 		case IKE_AUTH:
-			if (this->ike_sa->get_state(this->ike_sa) != IKE_ESTABLISHED)
+			if (!this->ike_sa->has_condition(this->ike_sa, COND_AUTHENTICATED))
 			{	/* wait until all authentication round completed */
 				return NEED_MORE;
 			}

src/libcharon/sa/ikev2/tasks/ike_auth.c
@@ -1167,15 +1167,8 @@ METHOD(task_t, build_r, status_t,
 							"%s.half_open_timeout", HALF_OPEN_IKE_SA_TIMEOUT,
 							lib->ns));
 	}
-	DBG0(DBG_IKE, "IKE_SA %s[%d] established between %H[%Y]...%H[%Y]",
-		 this->ike_sa->get_name(this->ike_sa),
-		 this->ike_sa->get_unique_id(this->ike_sa),
-		 this->ike_sa->get_my_host(this->ike_sa),
-		 this->ike_sa->get_my_id(this->ike_sa),
-		 this->ike_sa->get_other_host(this->ike_sa),
-		 this->ike_sa->get_other_id(this->ike_sa));
-	this->ike_sa->set_state(this->ike_sa, IKE_ESTABLISHED);
-	charon->bus->ike_updown(charon->bus, this->ike_sa, TRUE);
+
+	this->ike_sa->set_condition(this->ike_sa, COND_AUTHENTICATED, TRUE);
 	return SUCCESS;
 
 peer_auth_failed:
@@ -1504,15 +1497,8 @@ METHOD(task_t, process_i, status_t,
 				      "canceling");
 		goto peer_auth_failed;
 	}
-	DBG0(DBG_IKE, "IKE_SA %s[%d] established between %H[%Y]...%H[%Y]",
-		 this->ike_sa->get_name(this->ike_sa),
-		 this->ike_sa->get_unique_id(this->ike_sa),
-		 this->ike_sa->get_my_host(this->ike_sa),
-		 this->ike_sa->get_my_id(this->ike_sa),
-		 this->ike_sa->get_other_host(this->ike_sa),
-		 this->ike_sa->get_other_id(this->ike_sa));
-	this->ike_sa->set_state(this->ike_sa, IKE_ESTABLISHED);
-	charon->bus->ike_updown(charon->bus, this->ike_sa, TRUE);
+
+	this->ike_sa->set_condition(this->ike_sa, COND_AUTHENTICATED, TRUE);
 
 	if (this->redirect_to)
 	{

src/libcharon/sa/ikev2/tasks/ike_auth_lifetime.c
@@ -102,7 +102,7 @@ METHOD(task_t, build_r, status_t,
 	private_ike_auth_lifetime_t *this, message_t *message)
 {
 	if (message->get_exchange_type(message) == IKE_AUTH &&
-		this->ike_sa->get_state(this->ike_sa) == IKE_ESTABLISHED)
+		this->ike_sa->has_condition(this->ike_sa, COND_AUTHENTICATED))
 	{
 		if (!ike_sa_can_reauthenticate(this->ike_sa))
 		{
@@ -117,7 +117,7 @@ METHOD(task_t, process_i, status_t,
 	private_ike_auth_lifetime_t *this, message_t *message)
 {
 	if (message->get_exchange_type(message) == IKE_AUTH &&
-		this->ike_sa->get_state(this->ike_sa) == IKE_ESTABLISHED)
+		this->ike_sa->has_condition(this->ike_sa, COND_AUTHENTICATED))
 	{
 		process_payloads(this, message);
 		return SUCCESS;

commit afa4c29e22dc405dd1e73544d068f598009b9f6d

ike-sa: Fix handling of redirects during IKE_AUTH
Since the referenced commit, the state switch to IKE_ESTABLISHED doesn't
happen immediately after completing the authentication but only after
the remaining tasks have finished.  Due to that redirects during IKE_AUTH
were treated like those during IKE_SA_INIT causing the IKE_SA and task
manager to get reset.  However, unlike the ike-init task, the ike-auth
task returned SUCCESS and, therefore, got destroyed, while a different
task was removed from the array that was modified during the reset.
This later caused a dereference of the freed ike-auth task and a crash.

Fixes: 49a882644da1 ("ikev2: Trigger ike_updown() event after all IKE-specific tasks ran")

src/libcharon/sa/ike_sa.c
@@ -2380,7 +2380,11 @@ METHOD(ike_sa_t, handle_redirect, bool,
 	switch (this->state)
 	{
 		case IKE_CONNECTING:
-			return redirect_connecting(this, gateway);
+			if (!has_condition(this, COND_AUTHENTICATED))
+			{
+				return redirect_connecting(this, gateway);
+			}
+			/* fall-through during IKE_AUTH if authenticated */
 		case IKE_ESTABLISHED:
 			return redirect_established(this, gateway);
 		default: