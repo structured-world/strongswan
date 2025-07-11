structured-world/strongswan

commit 870ddf83c65568fb50c46b7a2f894ce91ab17bee

openssl: Fix testing KDF_PRF in the constructor with OpenSSL 3.5.1
Setting the salt to NULL now fails, so we set it to hash length's zeroes,
which is the default value for HKDF-Extract if no salt is passed.

Fixes strongswan/strongswan#2828

src/libstrongswan/plugins/openssl/openssl_kdf.c
@@ -201,6 +201,14 @@ kdf_t *openssl_kdf_create(key_derivation_function_t algo, va_list args)
 		.key = chunk_clone(chunk_from_str("00000000000000000000000000000000")),
 	);
 
+	/* also generate a salt (as if none was provided, i.e. zeroes of hash length)
+	 * as OpenSSL 3.5.1+ won't accept NULL anymore */
+	if (algo == KDF_PRF)
+	{
+		this->salt = chunk_copy_pad(chunk_alloc(get_length(this)),
+									chunk_empty, 0);
+	}
+
 	if (!this->hasher ||
 		!get_bytes(this, algo == KDF_PRF ? get_length(this) : sizeof(buf), buf))
 	{

commit aee5ba8298e6ec0d4accfe1f14e4c2afba082293

openssl: Don't allocate salt if PRF/hash is unknown
This can happen if e.g. AES-XCBC is selected.

Fixes: 870ddf83c655 ("openssl: Fix testing KDF_PRF in the constructor with OpenSSL 3.5.1")

src/libstrongswan/plugins/openssl/openssl_kdf.c
@@ -203,7 +203,7 @@ kdf_t *openssl_kdf_create(key_derivation_function_t algo, va_list args)
 
 	/* also generate a salt (as if none was provided, i.e. zeroes of hash length)
 	 * as OpenSSL 3.5.1+ won't accept NULL anymore */
-	if (algo == KDF_PRF)
+	if (algo == KDF_PRF && this->hasher)
 	{
 		this->salt = chunk_copy_pad(chunk_alloc(get_length(this)),
 									chunk_empty, 0);