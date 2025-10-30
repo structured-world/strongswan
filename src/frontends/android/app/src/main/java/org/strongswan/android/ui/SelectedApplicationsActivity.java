structured-world/strongswan

commit d533a73dde832b756a2ca0db954626162afb9bcb

android: Apply UI changes for edge-to-edge views in Android 15+
When targeting Android 15, edge-to-edge is the default and when targeting
Android 16, apps can't opt-out from this anymore.  So we update our views
and enable edge-to-edge also for older versions (avoids the black bar
behind the system UI at the bottom).  For most views we just use automatic
margins via android:fitsSystemWindows (or programmatically via
setDecorFitsSystemWindows).  However, for the profile lists and log views,
we take some extra measures that allow the lists to go behind the bottom
system UI.  Appropriate padding is applied at the bottom of the lists so
the last item(s) can be scrolled into full view.

src/frontends/android/app/src/main/java/org/strongswan/android/ui/SelectedApplicationsActivity.java
@@ -26,6 +26,7 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 import androidx.fragment.app.FragmentManager;
 
 public class SelectedApplicationsActivity extends AppCompatActivity
@@ -37,6 +38,8 @@ public class SelectedApplicationsActivity extends AppCompatActivity
 	protected void onCreate(@Nullable Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		WindowCompat.enableEdgeToEdge(getWindow());
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);

src/frontends/android/app/src/main/java/org/strongswan/android/ui/SettingsActivity.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 public class SettingsActivity extends AppCompatActivity
 {
@@ -28,6 +29,8 @@ public class SettingsActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		WindowCompat.enableEdgeToEdge(getWindow());
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 

src/frontends/android/app/src/main/java/org/strongswan/android/ui/TrustedCertificateImportActivity.java
@@ -41,6 +41,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.core.view.WindowCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 public class TrustedCertificateImportActivity extends AppCompatActivity

src/frontends/android/app/src/main/java/org/strongswan/android/ui/TrustedCertificatesActivity.java
@@ -33,6 +33,7 @@ import org.strongswan.android.logic.TrustedCertificateManager;
 import org.strongswan.android.logic.TrustedCertificateManager.TrustedCertificateSource;
 import org.strongswan.android.security.TrustedCertificateEntry;
 import org.strongswan.android.ui.CertificateDeleteConfirmationDialog.OnCertificateDeleteListener;
+import org.strongswan.android.utils.Utils;
 
 import java.security.KeyStore;
 
@@ -41,6 +42,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -71,6 +73,7 @@ public class TrustedCertificatesActivity extends AppCompatActivity implements Tr
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.trusted_certificates_activity);
+		WindowCompat.enableEdgeToEdge(getWindow());
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);

src/frontends/android/app/build.gradle
@@ -46,6 +46,7 @@ android {
 
 dependencies {
     implementation 'androidx.appcompat:appcompat:1.7.1'
+    implementation 'androidx.core:core:1.17.0'
     implementation 'androidx.lifecycle:lifecycle-process:2.9.4'
     implementation 'androidx.preference:preference:1.2.1'
     implementation 'com.google.android.material:material:1.13.0'

src/frontends/android/app/src/main/AndroidManifest.xml
@@ -36,6 +36,7 @@
         android:icon="@mipmap/ic_launcher"
         android:label="@string/app_name"
         android:theme="@style/ApplicationTheme"
+        android:windowSoftInputMode="adjustResize"
         android:networkSecurityConfig="@xml/network_security_config"
         android:enableOnBackInvokedCallback="true"
         android:allowBackup="false" >

src/frontends/android/app/src/main/java/org/strongswan/android/ui/LogActivity.java
@@ -26,10 +26,12 @@ import android.widget.Toast;
 import org.strongswan.android.R;
 import org.strongswan.android.data.LogContentProvider;
 import org.strongswan.android.logic.CharonVpnService;
+import org.strongswan.android.utils.Utils;
 
 import java.io.File;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 public class LogActivity extends AppCompatActivity
 {
@@ -38,6 +40,8 @@ public class LogActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.log_activity);
+		WindowCompat.enableEdgeToEdge(getWindow());
+		Utils.applyWindowInsetsAsMarginsForLists(findViewById(R.id.layout));
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}

src/frontends/android/app/src/main/java/org/strongswan/android/ui/LogFragment.java
@@ -30,6 +30,7 @@ import android.widget.ListView;
 
 import org.strongswan.android.R;
 import org.strongswan.android.logic.CharonVpnService;
+import org.strongswan.android.utils.Utils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -80,6 +81,8 @@ public class LogFragment extends Fragment
 		mLogAdapter = new LogAdapter(getActivity());
 		mLog = view.findViewById(R.id.log);
 		mLog.setAdapter(mLogAdapter);
+
+		Utils.applyWindowInsetsAsPaddingForLists(mLog);
 
 		mScrollPosition = -1;
 		if (savedInstanceState != null)

commit f4f1f314885b31ff5d1030bbc71a8441f9e4feb9

android: Fix edge-to-edge layout issues with settings and app selection
Using FrameLayout for the settings instead of the recommended
FragmentContainerView because the latter makes handling insets more
complicated (fitsSystemWindows doesn't work as the fragment is responsible
for handling insets but how that should work with PreferenceFragmentCompat
is unclear).

Fixes: d533a73dde83 ("android: Apply UI changes for edge-to-edge views in Android 15+")

src/frontends/android/app/src/main/java/org/strongswan/android/ui/SelectedApplicationsActivity.java
@@ -20,7 +20,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import org.strongswan.android.R;
 import org.strongswan.android.data.VpnProfileDataSource;
+import org.strongswan.android.utils.Utils;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
@@ -38,8 +40,9 @@ public class SelectedApplicationsActivity extends AppCompatActivity
 	protected void onCreate(@Nullable Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.selected_applications_activity);
 		WindowCompat.enableEdgeToEdge(getWindow());
-		WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+		Utils.applyWindowInsetsAsMarginsForLists(findViewById(R.id.fragment_container));
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
@@ -59,7 +62,7 @@ public class SelectedApplicationsActivity extends AppCompatActivity
 		if (mApps == null)
 		{
 			mApps = new SelectedApplicationsListFragment();
-			fm.beginTransaction().add(android.R.id.content, mApps, LIST_TAG).commit();
+			fm.beginTransaction().add(R.id.fragment_container, mApps, LIST_TAG).commit();
 		}
 	}
 

src/frontends/android/app/src/main/java/org/strongswan/android/ui/SelectedApplicationsListFragment.java
@@ -34,6 +34,7 @@ import org.strongswan.android.R;
 import org.strongswan.android.data.VpnProfileDataSource;
 import org.strongswan.android.ui.adapter.SelectedApplicationEntry;
 import org.strongswan.android.ui.adapter.SelectedApplicationsAdapter;
+import org.strongswan.android.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -60,6 +61,9 @@ public class SelectedApplicationsListFragment extends ListFragment implements Me
 	{
 		super.onViewCreated(view, savedInstanceState);
 		requireActivity().addMenuProvider(this, getViewLifecycleOwner());
+
+		getListView().setClipToPadding(false);
+		Utils.applyWindowInsetsAsPaddingForLists(getListView());
 
 		final boolean readOnly = getActivity().getIntent().getBooleanExtra(VpnProfileDataSource.KEY_READ_ONLY, false);
 		getListView().setChoiceMode(readOnly ? ListView.CHOICE_MODE_NONE : ListView.CHOICE_MODE_MULTIPLE);

src/frontends/android/app/src/main/java/org/strongswan/android/ui/SettingsActivity.java
@@ -19,6 +19,8 @@ package org.strongswan.android.ui;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import org.strongswan.android.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 
@@ -29,14 +31,18 @@ public class SettingsActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_activity);
 		WindowCompat.enableEdgeToEdge(getWindow());
-		WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		getSupportFragmentManager().beginTransaction()
-							.replace(android.R.id.content, new SettingsFragment())
-							.commit();
+		if (savedInstanceState == null)
+		{
+			getSupportFragmentManager().beginTransaction()
+				.setReorderingAllowed(true)
+				.add(R.id.fragment_container, SettingsFragment.class, null)
+				.commit();
+		}
 	}
 
 	@Override

src/frontends/android/app/src/main/java/org/strongswan/android/ui/TrustedCertificateImportActivity.java
@@ -41,7 +41,6 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.core.view.WindowCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 public class TrustedCertificateImportActivity extends AppCompatActivity

src/frontends/android/app/src/main/java/org/strongswan/android/ui/TrustedCertificatesActivity.java
@@ -33,7 +33,6 @@ import org.strongswan.android.logic.TrustedCertificateManager;
 import org.strongswan.android.logic.TrustedCertificateManager.TrustedCertificateSource;
 import org.strongswan.android.security.TrustedCertificateEntry;
 import org.strongswan.android.ui.CertificateDeleteConfirmationDialog.OnCertificateDeleteListener;
-import org.strongswan.android.utils.Utils;
 
 import java.security.KeyStore;
 

src/frontends/android/app/src/main/res/layout/selected_applications_activity.xml
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+    Copyright (C) 2025 Tobias Brunner
+
+    Copyright (C) secunet Security Networks AG
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+-->
+<androidx.fragment.app.FragmentContainerView
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:id="@+id/fragment_container"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent" />

src/frontends/android/app/src/main/res/layout/settings_activity.xml
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+    Copyright (C) 2025 Tobias Brunner
+
+    Copyright (C) secunet Security Networks AG
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+-->
+<FrameLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:id="@+id/fragment_container"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:fitsSystemWindows="true" />